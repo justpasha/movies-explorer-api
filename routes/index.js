9a551c864ec38fc21dbc69e996e8ebd483a211cc
Pavel Drobyshev <pavel.drob01@gmail.com>
2021-07-13 19:11:58 +0500

    fix: исправить валидацию

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.post(
     body: Joi.object().keys({
       email: Joi.string().required(),
       password: Joi.string().required(),
-      name: Joi.string().min(2).max(30),
+      name: Joi.string().min(2).max(30).required(),
     }),
   }),
   createUser
